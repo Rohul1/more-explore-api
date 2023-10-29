862133cb64878735b1d735e0381109d7d4e7e5fd
Rohul1 <rohul669319@gmail.com>
2023-10-30 00:41:10 +0600

    first commit

---
+++ kanye.js
@@ -0,0 +1,18 @@
+const loadQuote = ()=>{
+fetch("https://api.kanye.rest/")
+.then(res => res.json())
+.then(data => displayQuote(data));
+
+}
+
+
+
+
+const displayQuote = quote =>{
+
+const blockQoute = document.getElementById("quote");
+console.log(quote);
+blockQoute.innerText = quote.quote;
+
+
+}
